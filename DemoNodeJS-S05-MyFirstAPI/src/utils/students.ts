df50794790a0c6d4cebb5665e18b9a104f51ae18
HectorGarciaCarlota <soyunpenejaja@gmail.com>
2024-10-22 19:27:21 +0200

    Revert "feat: first commit"
    This reverts commit 6e505a4c

--- DemoNodeJS-S05-MyFirstAPI/src/utils/students.ts
+++
@@ -1,8 +0,0 @@
-const names: string[] = [
-  "Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Hector",
-  "Ivy", "Jack", "Karen", "Leo", "Megan", "Nina", "Oscar", "Paul",
-  "Quinn", "Rachel", "Sam", "Tina", "Ulysses", "Vera", "Wendy", "Xander",
-  "Yara", "Zane", "Liam", "Sophia"
-];
-
-export {names};
